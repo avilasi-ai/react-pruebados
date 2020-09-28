5db8c4f1e8c9ab76dbdba951c3dc642de9d290e9
Carlos Cova <ecommerce@avilasi.com>
2020-09-28 17:49:11 -0300

    primer commit

---
+++ docs/precache-manifest.fa31b5a20d48181d6f613fe24e1f6aef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9aeb5d88036ba9aa85eef25d05975fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc8ba79197499a55b4ed",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8ba79197499a55b4ed",
+    "url": "/static/js/main.5ef1ef48.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
